79d10d6a249461d4e5cfa5ceaab2b1740da7f181
EmmanuelBazan <emmanuelbazan99@gmail.com>
2024-07-19 20:43:50 -0600

    Update version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.bazan.devquiz"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
+        versionCode = 2
         versionName = "0.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
